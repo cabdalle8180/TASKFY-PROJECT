dd47ba1a599f3ef8de4c0dfe1a2534ecfbfcad83
cabdalle8180 <abdallamoha243@gmail.com>
2024-11-04 15:07:12 +0300

    update app.js

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const taskRoutes = require('./routes/taskRoutes');
 const HOSTNAME = 'localhost';
 const PORT = 9000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req , res) => {
+   
     if (req.url.startsWith('/tasks')) {
-        taskRoutes(req, res);
-    } else {
-        res.writeHead(404, "NOT FOUND", { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            message: "sorry you got lost"
-        }));
+        taskRoutes(req,res);
     }
+    else{
+        res.writeHead(404 ,"NOT FOUND " , {'Content-Type' : 'application/json'});
+        res.end(JSON.stringify({
+            message : "sorry you got lost"
+        }))
+    }
+  
 });
 
-server.listen(PORT, HOSTNAME, () => {
-    console.log(`The server is running on http://${HOSTNAME}:${PORT}`);
-});
+server.listen(PORT,HOSTNAME , ()=>{
+    console.log(`the server is runing  on http://${HOSTNAME}:${PORT}`);
+
+})
